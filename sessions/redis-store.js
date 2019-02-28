b861ad95836d08f8634bd931c654a19f81ef3a26
Ismail Arafa <ismailarafa18@gmail.com>
2019-02-27 22:48:48 -0500

    prep for prod

--- sessions/redis-store.js
+++ sessions/redis-store.js
@@ -3,5 +3,5 @@ const RedisStore = require("connect-redis")(session);
 require("dotenv").config();
 
 module.exports = new RedisStore({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
